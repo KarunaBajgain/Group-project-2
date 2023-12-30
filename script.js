8833eed4da814d675caf113872674e5e82c3cdfc
Karuna12 <133675390+KarunaBajgain@users.noreply.github.com>
2023-12-30 19:08:08 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+body {
+    font-family: 'Dancing Script', 'cursive';
+    margin: 0;
+    padding: 0;
+    background-image: url(https://wallpaperaccess.com/full/5165685.jpg);
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+}
+
+header {
+    background: transparent;
+    padding: 10px;
+    margin-left: 10%;
+    color: #782c57;
+    font-family: 'Dancing Script', 'cursive';
+    font-size: x-large;
+    position: fixed;
+    top: 0;
+    width: 100%;
+}
+
+img {
+    max-width: 20%; 
+    height: auto; 
+    float: left;
+    margin-left: 35%;
+    margin-top: 11.5em;
+    border-radius: 10%;
+    position: fixed;
+  
+}
+main {
+    max-width: 60%;
+    margin-top: 7em;
+    background: transparent;
+    margin-left: 10%;
+    padding: 10px;
+    color: #782c57;
+    font-size: x-large;
+    position: fixed;
+    top: 0;
+    width: 100%;
+}
+
+section {
+    margin-bottom: 20px;
+}
+
+label {
+    display: block;
+    margin-bottom: 5px;
+}
+
+input, button {
+    padding: 8px;
+    margin-bottom: 10px;
+}
+
+button {
+    background-color: #cd87ba;
+    color: #fff;
+    cursor: pointer;
+}
+
+#attendees-count {
+     background: transparent;
+     margin: 20px auto;
+     padding: 20px;
+     text-align: center;
+}
